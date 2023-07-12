da40c118d0d09712e2964b2aad315f694060853a
Ricardo Carriel <rick01.carriel@gmail.com>
2023-07-12 16:32:55 -0300

    Teste-06
    Banco de dados Alunos criado

---
+++ dbo.Aluno.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Aluno] (
+    [Id]            INT            NOT NULL,
+    [Nome]          NCHAR (100)    NOT NULL,
+    [Nota1]         DECIMAL (4, 2) NOT NULL,
+    [Nota2]         DECIMAL (4, 2) NOT NULL,
+    [AnoLancamento] INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
