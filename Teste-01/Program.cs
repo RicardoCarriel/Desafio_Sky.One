01ebacb5218c0174625d7e6aba571b09767172a1
Ricardo Carriel <rick01.carriel@gmail.com>
2023-07-12 13:36:29 -0300

    Initial commit

---
+++ Teste-01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a nota do primeiro semestre:");
+        double notaPrimeiroSemestre = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota do segundo semestre:");
+        double notaSegundoSemestre = double.Parse(Console.ReadLine());
+
+        double media = CalcularMedia(notaPrimeiroSemestre, notaSegundoSemestre);
+        Console.WriteLine("`Sua média é: " + media);
+    }
+
+    static double CalcularMedia(double notaPrimeiroSemestre, double notaSegundoSemestre)
+    {
+        return (notaPrimeiroSemestre + notaSegundoSemestre) / 2;
+    }
+}
