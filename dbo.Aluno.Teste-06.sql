adf5f5e75306042fba922a73bca8a131229b4a17
Ricardo Carriel <rick01.carriel@gmail.com>
2023-07-12 18:01:45 -0300

    Teste-06 Updated
    Alteração no banco (Id Incremental)

--- dbo.Aluno.Teste-06.sql
+++ dbo.Aluno.Teste-06.sql
@@ -1,9 +1,19 @@
-﻿CREATE TABLE [dbo].[Aluno] (
-    [Id]            INT            NOT NULL,
-    [Nome]          NCHAR (100)    NOT NULL,
+﻿USE [Desafio_Sky.One]
+GO
+
+/****** Object: Table [dbo].[Aluno] Script Date: 12/07/2023 18:00:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Aluno] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Nome]          NCHAR (40)     NOT NULL,
     [Nota1]         DECIMAL (4, 2) NOT NULL,
     [Nota2]         DECIMAL (4, 2) NOT NULL,
-    [AnoLancamento] INT            NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    [AnoLancamento] INT            NOT NULL
 );
+
 
