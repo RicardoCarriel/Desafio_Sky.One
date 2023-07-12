d1b00bc64f85612d0427e8abe2de76cd50a0f6e7
Ricardo Carriel <rick01.carriel@gmail.com>
2023-07-12 16:54:47 -0300

    Teste-08, Teste-09
    Teste-08 = Consultar informações dos alunos
    Teste-09 = Consultar informações dos alunos que a média anual for maior que 6.
    
    Refatoração dos nomes dos Testes 06 e 07

---
+++ dbo.Aluno.Teste-09.sql
@@ -0,0 +1,15 @@
+﻿USE [Desafio_Sky.One]
+GO
+SELECT
+    Nome,
+    Nota1,
+    Nota2,
+    AnoLancamento,
+    (Nota1 + Nota2) / 2 AS MediaAnual,
+    CASE
+        WHEN (Nota1 + Nota2) / 2 < 6.5 THEN 'Reprovado'
+        ELSE 'Aprovado'
+    END AS Status
+FROM Aluno
+WHERE (Nota1 + Nota2) / 2 > 6;
+
